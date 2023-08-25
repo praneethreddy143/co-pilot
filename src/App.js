b32bfe3237f4a0b384f6819d64e39f84b282550d
Reddy <praneeth.reddy-ext@ab-inbev.com>
2023-08-25 11:39:17 +0530

    Login Page component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import logo from './logo.svg';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="centered-box">
-      <div className="logo-container">
-      <div className="logo-divider">
-            <img src="logo.png" alt="Logo" className="logo" />
-          </div>
-          <span className="insight-copiot">insightcopiot</span>
-        </div>
-        <p className="login-para">Login</p>
-      <h2 className="text-above">Login with abinbev ID</h2>
-      <button className="login-button">Login</button>
-    </div>
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Login=()=>{
+    return (
+        <div className="centered-box">
+      <div className="logo-container">
+      <div className="logo-divider">
+            <img src="logo.png" alt="Logo" className="logo" />
+          </div>
+          <span className="insight-copiot">insightcopiot</span>
+        </div>
+        <p className="login-para">Login</p>
+      <h2 className="text-above">Login with abinbev ID</h2>
+      <button className="login-button">Login</button>
+    </div>
+    )
+}
+
+export default Login;
