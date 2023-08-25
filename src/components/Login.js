c6221777ba0eb18eb21fc9ce6c46305a3a75dddc
Reddy <praneeth.reddy-ext@ab-inbev.com>
2023-08-25 13:44:20 +0530

    Login Component updates

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,20 @@
 import React from "react";
-
-const Login=()=>{
+import logo from "../assets/AB_InBev_logo_PNG10.png";
+const Login = () => {
     return (
-        <div className="centered-box">
-      <div className="logo-container">
-      <div className="logo-divider">
-            <img src="logo.png" alt="Logo" className="logo" />
-          </div>
-          <span className="insight-copiot">insightcopiot</span>
+        <div className="centered-container">
+            <div className="centered-box">
+                <div className="logo-container">
+                    <div className="logo-divider">
+                        <img src={logo} alt="Logo" className="logo" />
+                    </div>
+                    <span className="insight-copiot">Insights Copiot</span>
+                </div>
+                <p className="login-para">Login</p>
+                <p className="text-above">please use your <span style={{color:"yellow"}}>AbinBevID</span> to login</p>
+                <button className="login-button">Login</button>
+            </div>
         </div>
-        <p className="login-para">Login</p>
-      <h2 className="text-above">Login with abinbev ID</h2>
-      <button className="login-button">Login</button>
-    </div>
     )
 }
 
